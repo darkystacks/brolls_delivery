71b0fc7c73690fe7f36b0d92c5dbbcbd17a0c093
darkystacks <darkymadeit@gmail.com>
2023-09-25 17:05:01 +0300

    now filter props saving to LS, small fixes, filter clear added, small fixes, project finished!

--- src/utils/getFilterFromLS.ts
+++ src/utils/getFilterFromLS.ts
@@ -1,8 +1,18 @@
-import { FilterSliceState } from '../redux/filter/types'
+import { FilterSliceState, SortPropertyEnum } from '../redux/filter/types'
 
 export const getFilterFromLS = () => {
 	const data: string | null = localStorage.getItem('filter')
-	const filter = data ? JSON.parse(data) : []
+	const filter = data
+		? JSON.parse(data)
+		: {
+				categoryId: 0,
+				searchValue: '',
+				currentPage: 1,
+				sort: {
+					name: 'популярности ↓',
+					sortProperty: SortPropertyEnum.RATING_DESC,
+				},
+		  }
 
 	return filter as FilterSliceState
 }
